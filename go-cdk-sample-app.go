1f2f4685c66d3e1108a51292c2f2e09e69b315f1
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-11-13 01:38:39 +0000

    reverting to init state

--- go-cdk-sample-app.go
+++ go-cdk-sample-app.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
 
-type CdkStackProps struct {
+type GoCdkSampleAppStackProps struct {
 	awscdk.StackProps
 }
 
-func NewCdkStack(scope constructs.Construct, id string, props *CdkStackProps) awscdk.Stack {
+func NewGoCdkSampleAppStack(scope constructs.Construct, id string, props *GoCdkSampleAppStackProps) awscdk.Stack {
 	var sprops awscdk.StackProps
 	if props != nil {
 		sprops = props.StackProps
@@ -21,9 +21,9 @@ func NewCdkStack(scope constructs.Construct, id string, props *CdkStackProps) aw
 	// The code that defines your stack goes here
 
 	// example resource
-	_ = awssqs.NewQueue(stack, jsii.String("CdkQueue"), &awssqs.QueueProps{
-		VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	})
+	// queue := awssqs.NewQueue(stack, jsii.String("GoCdkSampleAppQueue"), &awssqs.QueueProps{
+	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
+	// })
 
 	return stack
 }
@@ -33,7 +33,7 @@ func main() {
 
 	app := awscdk.NewApp(nil)
 
-	NewCdkStack(app, "CdkStack", &CdkStackProps{
+	NewGoCdkSampleAppStack(app, "GoCdkSampleAppStack", &GoCdkSampleAppStackProps{
 		awscdk.StackProps{
 			Env: env(),
 		},
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/go-micah/go-cdk-sample-app
 
-go 1.21.3
+go 1.18
 
 require (
 	github.com/aws/aws-cdk-go/awscdk/v2 v2.106.0
